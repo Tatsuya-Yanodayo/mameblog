7b0868a237d4f39101ac0d56d551e830f1107726
Tatsuya Yano <tatsuyanodayo@gmail.com>
2018-02-22 03:19:03 +0900

    [update]実験的にメインビジュアル下の境界線を波線に変更

--- wp-content/themes/mameblog/index.php
+++ wp-content/themes/mameblog/index.php
@@ -11,6 +11,14 @@
 
 <div id="arrow"><a href="#contents-wrap"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow.png"></a></div>
 <div id="contents-wrap">
+  <div id="svg_wrap">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1500 50" preserveAspectRatio="none" id="svg-bg1">
+      <path d="M0,50 v-25 q50,20 100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 v25 Z" fill="#191919"></path>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1500 50" preserveAspectRatio="none" id="svg-bg2">
+      <path d="M0,50 v-21 q50,20 100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 t100,0 v21 Z" fill="#e5e5e5"></path>
+    </svg>
+  </div>
   <div id="contents">
     <div id="posts">
 
