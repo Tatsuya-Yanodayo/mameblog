28997bfd0807b1ba9a4c73c22893042082876aff
Tatsuya Yano <tatsuyanodayo@gmail.com>
2018-03-04 21:56:04 +0900

    [add]ABOUTページの追加

--- wp-content/themes/mameblog/about.php
+++ wp-content/themes/mameblog/about.php
@@ -5,11 +5,76 @@ Template Name: about
 ?>
 
 
-<?php get_header(); ?>
-<div id="junbi">
-<div>
-  準備中..
-</div>
-</div>
 
-<?php get_footer(); ?>
+  <?php get_header(); ?>
+
+
+
+
+  <div id="about_head">
+    <h2>
+      ABOUT
+    </h2>
+    <span id="line"></span>
+  </div>
+
+
+  <div id="about_contents">
+    <div id="about_main_content">
+      <img id="about_logo_img" src="<?php echo get_template_directory_uri(); ?>/img/mainVisualLogo.png">
+      <div id="about_main_wrap">
+
+        <h3>Mission ルーツ・物語を紡ぎ、世界をもっと面白くする。</h3>
+
+        <p>ROOTBOX(ルーツボックス)は、travel photographer 松本真実が自分のルーツを探すために立ち上げたwebメディアです。2018年3月5日にローンチしました。
+        </p>
+        <p>私は物心ついた頃から、伝統的な民族衣装の形や柄、配色が大好きでした。バイクはSR、車ならROADSTARなど、歴史あるものに惹かれ、写真と出会えば古典技法など伝統的なものに興味を持ち、神話のような物語のある作品を作りたいと決意します。
+          <br> 旅が好きになり海外旅行へ行ってもすぐに都会を抜け出し、伝統的文化が残っていそうな田舎へ飛んで行ってしまいます。
+          <br> 次第に世界のルーツが私のルーツに繋がっているのでは、と思うようになりました。
+        </p>
+        <p>何故、惹かれてしまうのか、まだ自分でもわかりません。点と点を繋げるため、１から研究してキュレーションしてみたいと思う気持ちと、この知識は世界をもっと面白くすることができるかもしれないという思いが重なり、webメディアとして発信することにしました。いわば私にとってこのメディアを更新することは、自分探しの旅なのです。
+        </p>
+        <p>また、私が写真、webデザイン、ファッションデザインを専門としている為、デザイン、アート、ファッションを切り口に発信していく予定です。
+        </p>
+        <p>私の世界とあなたの世界をもっと面白くするメディアになれば幸いです。</p>
+
+      </div>
+    </div>
+
+    <span></span>
+
+    <div id="about_sub_contents">
+      <div id="about_web_design" class="about_sub_content">
+        <div class="about_sub_hl">
+          <h4>ABOUT WEB DESIGN</h4>
+        </div>
+        <p>新しく、斬新な切り口で伝統的な文化を発信したいので、webデザインも斬新さを取り入れました。全体の色は落ち着いた雰囲気にしたかったのでグレースケールで統一。 フラットデザインを採用し、その上動きや形で遊びを含ませています。
+          <br>メイン ビジュアルでは、ペトログリフを用い、海の向こうからの様々な文化の渡来を表しています。
+          <br> 記事の形は鍵穴を表し、使われている全ての下線は石器で削ったようにデザインしています。
+        </p>
+      </div>
+      <div id="about_auther" class="about_sub_content">
+        <div class="about_sub_hl">
+          <h4>ABOUT AUTHER</h4>
+        </div>
+        <div id="about_auther_content_wrap">
+          <div id="mami_image"><img src="<?php bloginfo('template_directory');?>/img/prof.jpg"></div>
+          <p>松本真実（Mami matsumoto）<br> 写真家、webデザイナー、ROOTBOX編集長、帽子デザイナー。
+            <br> 1993年大阪・枚方生まれ。
+            <br> 民族衣装、古着が好きで、2016年新卒で帽子メーカーに入社。
+            <br> 営業、企画、バイヤーを経験。
+            <br> 学生時代から独学で学んだ写真とwebデザインの知識を活かし、
+            <br> 2018年からフリーランス。
+            <br> 趣味はバイクとギターと旅とアニメ。
+            <a href="http://mami-matsumoto.com/">→&nbsp&nbspview portfolio</a>
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
+
+
+
+
+  <?php get_footer(); ?>
