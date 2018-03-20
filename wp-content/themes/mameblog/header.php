53c5c44a7d17c49b08518ba35eab9cb95864fdc3
Tatsuya Yano <tatsuyanodayo@gmail.com>
2018-03-20 15:00:19 +0900

    [fix]GoogleアナリティクスをGitの管理外に移動 手動でanalytics.phpをアップロードします

--- wp-content/themes/mameblog/header.php
+++ wp-content/themes/mameblog/header.php
@@ -3,19 +3,7 @@
 
 <head>
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115657536-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'UA-115657536-1');
-
-  </script>
+  <?php get_template_part('analytics'); ?>
   
   <meta charset="utf-8">
 
