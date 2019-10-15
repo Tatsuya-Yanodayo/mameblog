2e53e8d652452153d1628d4e0b6d1909d1709144
matsumoto-mami <mame4630@gmail.com>
2019-10-15 13:51:27 +0900

    add pankuzu list

--- wp-content/themes/mameblog/category.php
+++ wp-content/themes/mameblog/category.php
@@ -1,10 +1,41 @@
+
+
 <?php get_header(); ?>
+
+
+<?php
+  $cat_tag_list_html = '<ul class="tag-list">';
+
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+    ?>
+
+
+        <?php $cat_tag_list_html += get_the_tag_list('<li>','</li><li>','</li>'); ?>
+        <?php error_log(get_the_tag_list('<li>','</li><li>','</li>'),3,"./test.log"); ?>
+
+        <?php
+        endwhile;
+        else:
+      ?>
+
+          <?php endif; ?>
+
+      <?php $cat_tag_list_html += "</ul>" ?>
+
+
+
 
 <div id="cat_info_wrap">
   <h2 id="cat_name">
     <?php single_cat_title(); ?>
   </h2>
   <span id="line"></span>
+
+  <div class="tag-list-wrap">
+    <?php echo $cat_tag_list_html ?>
+      <?php the_tags('<ul class="tag-list"><li>','</li><li>','</li></ul>'); ?>
+    </div> 
+
  
 </div>
 <div id="contents-wrap">
